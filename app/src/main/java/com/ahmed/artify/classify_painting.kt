148640b4bc040b65874332c7e14e2773cf9a964d
killernumber1 <ahmedgamerpes2016@gmail.com>
2024-02-02 19:45:44 +0200

    Classify Art Activity added

---
+++ app/src/main/java/com/ahmed/artify/classify_painting.kt
@@ -0,0 +1,11 @@
+package com.ahmed.artify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class classify_painting : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_classify_painting)
+    }
+}
