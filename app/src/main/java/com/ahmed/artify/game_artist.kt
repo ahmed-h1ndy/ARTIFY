4db7b40d9931271ee2e6ae585421eff44ef6b80f
killernumber1 <ahmedgamerpes2016@gmail.com>
2024-02-06 14:24:50 +0200

    Game activity added Artist Game activity added small style adjustments

---
+++ app/src/main/java/com/ahmed/artify/game_artist.kt
@@ -0,0 +1,11 @@
+package com.ahmed.artify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class game_artist : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_artist)
+    }
+}
